7b3b95b731c6b5f6303348ab071ee962ea46f3fd
timstellar <timstellar.official@gmail.com>
2024-09-28 12:59:25 +0300

    Variant 20

---
+++ Lab1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+// Variant 20
+int main(int argc, char* argv[]) {
+    int a{};
+    int b{};
+    int c{};
+    int d{};
+
+    std::vector<std::string> positive;
+    std::vector<std::string> negative;
+
+    std::cout << "Enter number A: ";
+    std::cin >> a;
+    std::cout << "Enter number B: ";
+    std::cin >> b;
+    std::cout << "Enter number C: ";
+    std::cin >> c;
+    std::cout << "Enter number D: ";
+    std::cin >> d;
+
+    if (a >= 0) {
+        positive.push_back("A");
+    } else {
+        negative.push_back("A");
+    }
+    if (b >= 0) {
+        positive.push_back("B");
+    } else {
+        negative.push_back("B");
+    }
+    if (c >= 0) {
+        positive.push_back("C");
+    } else {
+        negative.push_back("C");
+    }
+    if (d >= 0) {
+        positive.push_back("D");
+    } else {
+        negative.push_back("D");
+    }
+
+    std::cout << "Positive numbers: ";
+    for (std::string &i : positive) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::cout << "Negative numbers: ";
+    for (std::string &i : negative) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
